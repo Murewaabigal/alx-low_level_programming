2250e4ed27f8e4f23d85b37a8a31e383c3e7fd50
murewaabigal <murewaabigal@gmail.com>
2022-03-17 02:56:54 -0700

    commit this

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet - prints the alphabet, in lowercase.
+* Return: Always 0.
+*/
+
+void print_alphabet(void)
+{
+char i = 'a';
+
+while (i <= 'z')
+{
+	_putchar(i);
+	i++;
+}
+_putchar('/n');
+}
+	 
+
