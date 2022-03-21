de34ffb4368d8927132668d25033a29b33f85aac
murewaabigal <murewaabigal@gmail.com>
2022-03-21 14:35:01 -0700

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,20 +1,13 @@
-#include "main.h"
+#include "stdio.h"
 
-void reset_to_98(int *n) {
-	  *n = 98;
-}
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+  * reset_to_98 - Updates the value it points to 98
+  * @n: pointer to n
+  *
+  * Return: void
+  */
+int main (void)
 {
-	int n;
-
-	n = 402;
-	_putchar("n=%d\n", n);
-	reset_to_98(&n);
-	_putchar("n=%d\n" ,n)
-	return (0);
+int *n;
+*n = 98;
 }	
